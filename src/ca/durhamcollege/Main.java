6669989353f3d1123ee18848fd361f44835f5ac1
Jacob Kent <jacobgkent@gmail.com>
2021-11-10 17:10:22 -0500

    Added basic age validation

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -17,14 +17,43 @@ public class Main
         // Set up Scanner
         Scanner keyboard = new Scanner(System.in);
 
-        // Input
+        // Declarations
         String name = "";
+        float age = 0.0f;
+        final float minimumAge = 0.0f;
+        final float maximumAge = 120.0f;
+
+        boolean validAge = false;
+
+        // Input
         System.out.print("Please enter your name: ");
         name = keyboard.nextLine();
 
-        float age = 0.0f;
-        System.out.print("Please enter your age: ");
-        age = keyboard.nextFloat();
+
+        do
+        {
+            System.out.print("Please enter your age: ");
+            try
+            {
+                age = keyboard.nextFloat();
+                if(age <= minimumAge || age >= maximumAge)
+                {
+                    System.out.println("Error: Age must be within range between 0.0 and 120.0.");
+                    keyboard.nextLine();
+                }
+                else
+                {
+                    validAge = true;
+                }
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error: Invalid input for floating-point number.");
+                keyboard.nextLine();
+            }
+
+        }
+        while(!validAge);
 
         // Output
         System.out.println();
