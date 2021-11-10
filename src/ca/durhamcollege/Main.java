d86517c2a10361988f4d90356d77971af82c9a3c
Jacob Kent <jacobgkent@gmail.com>
2021-11-10 16:31:48 -0500

    Added initial comment and basic input/output test

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,8 +1,24 @@
+/**
+ * @author Jacob Kent
+ * @studentID 100609784
+ * @version 0.0.1
+ * @date Nov 10, 2021
+ */
+
 package ca.durhamcollege;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String name = "";
+        int age = 0;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        name = keyboard.nextLine();
+        System.out.printf("You entered: %s", name);
     }
 }
